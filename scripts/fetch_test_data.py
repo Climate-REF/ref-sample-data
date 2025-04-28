a5379b49de9f3ee6195139abe873e3305beceaec
Jared Lewis <jared@jared.kiwi.nz>
2025-04-28 12:51:30 +1000

    chore: Remove old files

--- scripts/fetch_test_data.py
+++ scripts/fetch_test_data.py
@@ -71,6 +71,7 @@ def process_sample_data_request(
     for _, dataset in datasets.iterrows():
         match = _get_match(processed_datasets, request.source_type, dataset.key)
 
+        # Check if the dataset has already been processed and can be skipped
         if match is not None and request.time_span is not None:
             # Dataset has already been processed and a time span was specified
             # Check if the dataset already covers the requested time span
@@ -91,7 +92,13 @@ def process_sample_data_request(
             request.time_span = (str(time_start), str(time_end))
 
             logger.info(f"Regenerating dataset with new time span: {dataset.key} {request.time_span}")
+            for file in match.files:
+                file_path = pathlib.Path(file)
+                if file_path.exists():
+                    logger.info(f"Removing existing file: {file}")
+                    file_path.unlink()
 
+        output_filenames = []
         for ds_filename in dataset["files"]:
             ds_orig = xr.open_dataset(ds_filename)
 
@@ -105,10 +112,12 @@ def process_sample_data_request(
             output_filename = output_directory / request.generate_filename(dataset, ds_decimated, ds_filename)
             output_filename.parent.mkdir(parents=True, exist_ok=True)
             ds_decimated.to_netcdf(output_filename)
+            output_filenames.append(output_filename)
 
         item = {
             "source_type": request.source_type,
             "key": dataset.key,
+            "files": output_filenames,
         }
         if request.time_span is not None:
             item["time_start"] = request.time_span[0]
@@ -255,7 +264,7 @@ def create_sample_data(
     output: Path = OUTPUT_PATH,
 ) -> None:
     """Fetch and create sample datasets"""
-    processed_datasets = pd.DataFrame(columns=["source_type", "key", "time_start", "time_end"])
+    processed_datasets = pd.DataFrame(columns=["source_type", "key", "files", "time_start", "time_end"])
 
     for dataset_requested in DATASETS_TO_FETCH:
         # Process the request
