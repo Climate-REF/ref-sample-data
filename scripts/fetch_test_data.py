8fde3226314a4a8bdc400fcdc99a5ffbddcdd8f3
Jared Lewis <jared@jared.kiwi.nz>
2025-05-16 15:14:57 +1000

    chore: Use new api

--- scripts/fetch_test_data.py
+++ scripts/fetch_test_data.py
@@ -7,7 +7,7 @@ import typer
 import xarray as xr
 from loguru import logger
 
-from ref_sample_data import CMIP6Request, DataRequest, Obs4MIPsRequest, Obs4REFRequest
+from ref_sample_data import DataRequest, Obs4REFRequest
 
 OUTPUT_PATH = Path("data")
 app = typer.Typer()
@@ -131,150 +131,150 @@ def process_sample_data_request(
 
 
 DATASETS_TO_FETCH = [
-    # Example metric data
-    CMIP6Request(
-        facets=dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "tas", "tos", "rsut", "rlut", "rsdt"],
-            experiment_id=["ssp126", "historical"],
-        ),
-        remove_ensembles=True,
-        time_span=("2000", "2025"),
-    ),
-    # ESMValTool Climate at global warmings levels data
-    CMIP6Request(
-        facets=dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "pr", "tas"],
-            experiment_id=["ssp126", "historical"],
-        ),
-        remove_ensembles=True,
-        time_span=("1850", "2100"),
-    ),
-    # ESMValTool Cloud radiative effects
-    CMIP6Request(
-        facets=dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "rlut", "rlutcs", "rsut", "rsutcs"],
-            experiment_id="historical",
-        ),
-        remove_ensembles=True,
-        time_span=("2005", "2014"),
-    ),
-    # ESMValTool ECS data
-    CMIP6Request(
-        facets=dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "rlut", "rsdt", "rsut", "tas"],
-            experiment_id=["abrupt-4xCO2", "piControl"],
-        ),
-        remove_ensembles=True,
-        time_span=("0101", "0125"),
-    ),
-    # ESMValTool TCR data
-    CMIP6Request(
-        facets=dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "tas"],
-            experiment_id=["1pctCO2", "piControl"],
-        ),
-        remove_ensembles=True,
-        time_span=("0101", "0180"),
-    ),
-    # ESMValTool TCRE data
-    CMIP6Request(
-        facets=dict(
-            source_id="MPI-ESM1-2-LR",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "fco2antt", "tas"],
-            experiment_id=["esm-1pctCO2"],
-        ),
-        remove_ensembles=True,
-        time_span=("1850", "1915"),
-    ),
-    CMIP6Request(
-        facets=dict(
-            source_id="MPI-ESM1-2-LR",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "tas"],
-            experiment_id=["esm-piControl"],
-        ),
-        remove_ensembles=True,
-        time_span=("1850", "1915"),
-    ),
-    # ESMValTool ZEC data
-    CMIP6Request(
-        facets=dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "tas"],
-            experiment_id=["1pctCO2", "esm-1pct-brch-1000PgC"],
-        ),
-        remove_ensembles=True,
-        time_span=("0158", "0268"),
-    ),
-    # ESMValTool Sea Ice Area Seasonal Cycle data
-    CMIP6Request(
-        facets=dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacello", "siconc"],
-            experiment_id=["historical"],
-        ),
-        remove_ensembles=True,
-        time_span=("1979", "2014"),
-    ),
-    # ILAMB data
-    CMIP6Request(
-        facets=dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "sftlf", "gpp", "pr", "tas", "mrro", "mrsos", "cSoil", "lai", "nbp"],
-            experiment_id=["historical"],
-        ),
-        remove_ensembles=True,
-        time_span=("2000", "2025"),
-    ),
-    # IOMB data
-    CMIP6Request(
-        facets=dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacello", "sftof", "tos", "sos", "msftmz"],
-            experiment_id=["historical"],
-        ),
-        remove_ensembles=True,
-        time_span=("2000", "2025"),
-    ),
-    # PMP modes of variability data
-    CMIP6Request(
-        facets=dict(
-            source_id="ACCESS-ESM1-5",
-            frequency=["fx", "mon"],
-            variable_id=["areacella", "ts", "psl"],
-            experiment_id=["historical", "hist-GHG"],
-            variant_label=["r1i1p1f1", "r2i1p1f1"],
-        ),
-        remove_ensembles=False,
-        time_span=("2000", "2025"),
-    ),
-    # Obs4MIPs AIRS data
-    Obs4MIPsRequest(
-        facets=dict(
-            project="obs4MIPs",
-            institution_id="NASA-JPL",
-            frequency="mon",
-            source_id="AIRS-2-1",
-            variable_id="ta",
-        ),
-        remove_ensembles=False,
-        time_span=("2002", "2016"),
-    ),
+    # # Example metric data
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="ACCESS-ESM1-5",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacella", "tas", "tos", "rsut", "rlut", "rsdt"],
+    #         experiment_id=["ssp126", "historical"],
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("2000", "2025"),
+    # ),
+    # # ESMValTool Climate at global warmings levels data
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="ACCESS-ESM1-5",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacella", "pr", "tas"],
+    #         experiment_id=["ssp126", "historical"],
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("1850", "2100"),
+    # ),
+    # # ESMValTool Cloud radiative effects
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="ACCESS-ESM1-5",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacella", "rlut", "rlutcs", "rsut", "rsutcs"],
+    #         experiment_id="historical",
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("2005", "2014"),
+    # ),
+    # # ESMValTool ECS data
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="ACCESS-ESM1-5",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacella", "rlut", "rsdt", "rsut", "tas"],
+    #         experiment_id=["abrupt-4xCO2", "piControl"],
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("0101", "0125"),
+    # ),
+    # # ESMValTool TCR data
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="ACCESS-ESM1-5",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacella", "tas"],
+    #         experiment_id=["1pctCO2", "piControl"],
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("0101", "0180"),
+    # ),
+    # # ESMValTool TCRE data
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="MPI-ESM1-2-LR",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacella", "fco2antt", "tas"],
+    #         experiment_id=["esm-1pctCO2"],
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("1850", "1915"),
+    # ),
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="MPI-ESM1-2-LR",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacella", "tas"],
+    #         experiment_id=["esm-piControl"],
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("1850", "1915"),
+    # ),
+    # # ESMValTool ZEC data
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="ACCESS-ESM1-5",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacella", "tas"],
+    #         experiment_id=["1pctCO2", "esm-1pct-brch-1000PgC"],
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("0158", "0268"),
+    # ),
+    # # ESMValTool Sea Ice Area Seasonal Cycle data
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="ACCESS-ESM1-5",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacello", "siconc"],
+    #         experiment_id=["historical"],
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("1979", "2014"),
+    # ),
+    # # ILAMB data
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="ACCESS-ESM1-5",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacella", "sftlf", "gpp", "pr", "tas", "mrro", "mrsos", "cSoil", "lai", "nbp"],
+    #         experiment_id=["historical"],
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("2000", "2025"),
+    # ),
+    # # IOMB data
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="ACCESS-ESM1-5",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacello", "sftof", "tos", "sos", "msftmz"],
+    #         experiment_id=["historical"],
+    #     ),
+    #     remove_ensembles=True,
+    #     time_span=("2000", "2025"),
+    # ),
+    # # PMP modes of variability data
+    # CMIP6Request(
+    #     facets=dict(
+    #         source_id="ACCESS-ESM1-5",
+    #         frequency=["fx", "mon"],
+    #         variable_id=["areacella", "ts", "psl"],
+    #         experiment_id=["historical", "hist-GHG"],
+    #         variant_label=["r1i1p1f1", "r2i1p1f1"],
+    #     ),
+    #     remove_ensembles=False,
+    #     time_span=("2000", "2025"),
+    # ),
+    # # Obs4MIPs AIRS data
+    # Obs4MIPsRequest(
+    #     facets=dict(
+    #         project="obs4MIPs",
+    #         institution_id="NASA-JPL",
+    #         frequency="mon",
+    #         source_id="AIRS-2-1",
+    #         variable_id="ta",
+    #     ),
+    #     remove_ensembles=False,
+    #     time_span=("2002", "2016"),
+    # ),
     # All unpublished obs4mips datasets
     Obs4REFRequest(),
 ]
