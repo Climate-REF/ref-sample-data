b2634bb2506837d10a3af9ed760d98bcacf669df
Bouwe Andela <b.andela@esciencecenter.nl>
2025-08-20 11:11:27 +0200

    Show intake-esgf log messages and update intake-esgf

--- scripts/fetch_test_data.py
+++ scripts/fetch_test_data.py
@@ -1,3 +1,5 @@
+import inspect
+import logging
 import pathlib
 from pathlib import Path
 
@@ -423,6 +425,32 @@ DATASETS_TO_FETCH = [
 ]
 
 
+# Copied from https://github.com/Delgan/loguru#entirely-compatible-with-standard-logging
+class InterceptHandler(logging.Handler):
+    """Intercepts standard logging messages and redirects them to Loguru."""
+
+    def emit(self, record: logging.LogRecord) -> None:
+        """Emit a record."""
+        # Get corresponding Loguru level if it exists.
+        try:
+            level: str | int = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message.
+        frame, depth = inspect.currentframe(), 0
+        while frame:
+            filename = frame.f_code.co_filename
+            is_logging = filename == logging.__file__
+            is_frozen = "importlib" in filename and "_bootstrap" in filename
+            if depth > 0 and not (is_logging or is_frozen):
+                break
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth + 2, exception=record.exc_info).log(level, record.getMessage())
+
+
 @app.command()
 def create_sample_data(
     decimate: bool = True,
@@ -432,6 +460,7 @@ def create_sample_data(
     run_sequentially: bool = False,
 ) -> None:
     """Fetch and create sample datasets"""
+    logging.basicConfig(handlers=[InterceptHandler()], force=True)
     processed_datasets = pd.DataFrame(columns=["source_type", "key", "files", "time_start", "time_end"])
 
     if run_sequentially:
