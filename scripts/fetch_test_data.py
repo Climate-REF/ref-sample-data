ebb77df34a22c57c2971e92f51dfe36443d876fc
Jared Lewis <jared@jared.kiwi.nz>
2025-09-26 13:33:11 +1000

    chore: Add PMP ENSO datasets

--- scripts/fetch_test_data.py
+++ scripts/fetch_test_data.py
@@ -485,18 +485,31 @@ DATASETS_TO_FETCH = [
         remove_ensembles=False,
         time_span=("2000", "2025"),
     ),
-    # # Obs4MIPs AIRS data
-    # Obs4MIPsRequest(
-    #     facets=dict(
-    #         project="obs4MIPs",
-    #         institution_id="NASA-JPL",
-    #         frequency="mon",
-    #         source_id="AIRS-2-1",
-    #         variable_id="ta",
-    #     ),
-    #     remove_ensembles=False,
-    #     time_span=("2002", "2016"),
-    # ),
+    # PMP ENSO data
+    CMIP6Request(
+        id="pmp-enso",
+        facets=dict(
+            source_id="ACCESS-ESM1-5",
+            frequency=["fx", "mon"],
+            variable_id=[
+                "areacella",
+                "sftlf",
+                "ts",
+                "tauu",
+                "taux",
+                "hfls",
+                "hfss",
+                "rlds",
+                "rlus",
+                "rsds",
+                "rsus",
+            ],
+            experiment_id=["historical"],
+            variant_label=["r1i1p1f1"],
+        ),
+        remove_ensembles=False,
+        time_span=("2000", "2025"),
+    ),
     # All unpublished obs4mips datasets
     Obs4REFRequest(),
 ]
